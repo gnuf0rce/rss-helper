b7a1227177c945f330b136c52bb615d19a6385cb
cssxsh <cssxsh@gmail.com>
2022-08-07 21:07:02 +0800

    change: publish info

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ version = "1.2.3"
 mavenCentralPublish {
     useCentralS01()
     singleDevGithubProject("gnuf0rce", "rss-helper", "cssxsh")
-    licenseFromGitHubProject("AGPL-3.0", "master")
+    licenseFromGitHubProject("AGPL-3.0")
     workingDir = System.getenv("PUBLICATION_TEMP")?.let { file(it).resolve(projectName) }
         ?: project.buildDir.resolve("publishing-tmp")
     publication {
