a4c5a2d4e82172de9f709ad4ed80dab6d86525aa
cssxsh <cssxsh@gmail.com>
2024-03-09 08:55:52 +0800

    update: dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.serialization") version "1.8.22"
 
-    id("net.mamoe.mirai-console") version "2.15.0"
-    id("me.him188.maven-central-publish") version "1.0.0-dev-3"
+    id("net.mamoe.mirai-console") version "2.16.0"
+    id("me.him188.maven-central-publish") version "1.0.0"
 }
 
 group = "io.github.gnuf0rce"
@@ -26,25 +26,28 @@ repositories {
 }
 
 dependencies {
-    implementation("com.rometools:rome:2.1.0")
-    implementation("com.rometools:rome-modules:2.1.0")
-    implementation("org.jsoup:jsoup:1.16.1")
+    implementation("org.jsoup:jsoup:1.17.2")
     testImplementation(kotlin("test"))
     //
-    implementation(platform("net.mamoe:mirai-bom:2.15.0"))
-    compileOnly("net.mamoe:mirai-console-compiler-common")
+    implementation(platform("net.mamoe:mirai-bom:2.16.0"))
+    testImplementation("net.mamoe:mirai-console-compiler-common")
+    testImplementation("net.mamoe:mirai-core-mock")
     testImplementation("net.mamoe:mirai-logging-slf4j")
     //
-    implementation(platform("io.ktor:ktor-bom:2.3.5"))
+    implementation(platform("io.ktor:ktor-bom:2.3.9"))
     implementation("io.ktor:ktor-client-okhttp")
     implementation("io.ktor:ktor-client-encoding")
     implementation("io.ktor:ktor-client-content-negotiation")
     implementation("io.ktor:ktor-serialization-kotlinx-json")
     //
-    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.11.0"))
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.12.0"))
     implementation("com.squareup.okhttp3:okhttp-dnsoverhttps")
     //
-    implementation(platform("org.slf4j:slf4j-parent:2.0.9"))
+    implementation(platform("com.rometools:rome-parent:2.1.0"))
+    implementation("com.rometools:rome")
+    implementation("com.rometools:rome-modules")
+    //
+    implementation(platform("org.slf4j:slf4j-parent:2.0.12"))
     testImplementation("org.slf4j:slf4j-simple")
 }
 
